baa316e3295b88c8c1e918355a5f3a4d3346cea2
Mrabdulbtw <abdulofficialbtw@gmail.com>
2023-06-09 23:21:57 +0530

    style

--- src/projectsapi.js
+++ src/projectsapi.js
@@ -1,6 +1,7 @@
 import cart from "./assets/portfolio.png"
 
 
+
 export const projectsData = [
   {
     projectImg: cart,
@@ -8,6 +9,14 @@ export const projectsData = [
     projectsdescription: `this is simpel E-commerc website,used metrial of this project React ,useContext.
       useContext was importent of dataManipulation in this project.then vanilla css used to this project. hosting platform github.com...`,
     projectLink: `https://mrabdulbtw.github.io/carte/`,
+    githublink:`https://github.com/Mrabdulbtw`
+  },
+  {
+    projectImg: cart,
+    projectName: 'crypto-cheaker',
+    projectsdescription: `this is simpel E-commerc website,used metrial of this project React ,useContext.
+      useContext was importent of dataManipulation in this project.then vanilla css used to this project. hosting platform github.com...`,
+    projectLink: `https://mrabdulbtw.github.io/crypto-cheaker`,
     githublink:`https://github.com/Mrabdulbtw`
   },
 ] 
